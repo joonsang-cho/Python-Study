0ab9e5082de3e9b12dfb6a4f90e17c88b78fd504
macbook <1234>
2020-02-07 15:50:56 +0900

    questions added

--- scratches/HW2.py
+++ scratches/HW2.py
@@ -1,3 +1,8 @@
+#Write a Python program to check whether a specified value is contained in a group of values.
+#Test Data :
+#3 -> [1, 5, 8, 3] : True
+#-1 -> [1, 5, 8, 3] : False
+
 def check(n):
     a = [1, 5, 8, 3]
     if n in a:
--- scratches/HW4.py
+++ scratches/HW4.py
@@ -1,3 +1,6 @@
+#Write a Python program to create all possible strings
+#by using 'a', 'e', 'i', 'o', 'u'. Use the characters exactly once.
+
 text = ['a', 'e', 'i', 'o', 'u']
 
 while text:
