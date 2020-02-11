3762a0e70b5dbbe2e1422d799292322a62e5d8ef
macbook <1234>
2020-02-11 15:40:47 +0900

    hw 4,5,6 solved

--- scratches/HW3.py
+++ scratches/HW3.py
@@ -11,14 +11,14 @@ numbers = [386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 
            399, 162, 758, 219, 918, 237, 412, 566, 826, 248, 866, 950, 626, 949, 687, 217,
            815, 67, 104, 58, 512, 24, 892, 894, 767, 553, 81, 379, 843, 831, 445, 742, 717,
            958, 743, 527]
-list=[]
+done = []
 if __name__ == '__main__':
     for num in numbers:
         if num == 237:
             break
         else:
             if num % 2 == 0:
-                list.append(num)
+                done.append(num)
             else:
                 continue
-    print(list)
+    print(done)
